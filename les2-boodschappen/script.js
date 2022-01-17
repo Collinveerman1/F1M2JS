437fb8d02d30afb34f47edce1e0efc59731098d6
Collinveerman1 <collinveerman1@gmail.com>
2022-01-17 12:23:18 +0100

    werkende koelkast

--- les2-boodschappen/script.js
+++ les2-boodschappen/script.js
@@ -1,1 +1,22 @@
 console.log("Script is geladen!");
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit');
+
+function zetInGroenteLa(groente){
+    console.log(groente)
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    nieuwElement.className = 'groen';
+    groentevak.appendChild(nieuwElement);
+}
+
+function zetInFruitLa(fruit){
+    console.log(fruit)
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = 'rood';
+    fruitvak.appendChild(nieuwElement);
+}
